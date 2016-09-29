996db8ac5b0ca819d2e73723cc15da14fa624eef
AviliaXIIIR407 <liaaviliaistiqomah@gmail.com>
2016-09-29 18:51:54 +0700

    Pembetulan Deklarasi dll

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl4008/rentalbuku/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl4008/rentalbuku/MainActivity.java
@@ -7,16 +7,18 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     EditText etNama, etAlamat, etKtp;
     RadioButton rbSM, rbGM, rbPM;
+    RadioGroup rgMember;
     Spinner spJudul;
     CheckBox cbCC, cbBS, cbPB;
     Button bOk;
-    TextView tvHasil;
+    TextView tvHasilsp, tvHasilcb, tvHasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +29,12 @@ public class MainActivity extends AppCompatActivity {
         etAlamat = (EditText) findViewById(R.id.editTextAlamat);
         etKtp = (EditText) findViewById(R.id.editTextKtp);
 
+        bOk = (Button) findViewById(R.id.buttonOk);
         rbSM = (RadioButton) findViewById(R.id.radioButtonSM);
         rbGM = (RadioButton) findViewById(R.id.radioButtonGM);
         rbPM = (RadioButton) findViewById(R.id.radioButtonPM);
+
+        rgMember = (RadioGroup) findViewById(R.id.radioGroupMember);
 
         spJudul = (Spinner) findViewById(R.id.spinnerJudul);
 
@@ -37,7 +42,9 @@ public class MainActivity extends AppCompatActivity {
         cbBS = (CheckBox) findViewById(R.id.checkBoxBS);
         cbPB = (CheckBox) findViewById(R.id.checkBoxPB);
 
-        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        tvHasilsp = (TextView) findViewById(R.id.textViewHasil);
+        tvHasilcb = (TextView) findViewById(R.id.textViewHasil2);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil3);
 
         findViewById(R.id.buttonOk).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,15 +53,53 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        bOk.setOnClickListener(new View.OnClickListener() {
+        /*bOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 doProcess();
             }
-        });
+        });*/
     }
 
     private void doClick() {
+
+        int diskon = 0;
+        RadioButton rb = (RadioButton)
+                findViewById(rgMember.getCheckedRadioButtonId());
+        if (rb.getText().toString().equals("Silver Member")) diskon = 10;
+        else if (rb.getText().toString().equals("Gold Member")) diskon = 20;
+        else diskon = 30;
+
+        if (isValid()) {
+            String nama = etNama.getText().toString();
+            String alamat = etAlamat.getText().toString();
+            String ktp = etKtp.getText().toString();
+
+            tvHasil.setText("Nama : " + nama + "\n Alamat : " + alamat + "\n No Ktp : " + ktp);
+        }
+
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("Nama : ");
+        builder.append(etNama.getText().toString() + "\n");
+        builder.append("Alamat : ");
+        builder.append(etAlamat.getText().toString() + "\n");
+        builder.append("No. Ktp : ");
+        builder.append(etKtp.getText().toString() + "\n");
+        builder.append("Member : ");
+        builder.append(rb.getText().toString() + "\n");
+        builder.append("Judul Buku : ");
+        builder.append(tvHasilsp.getText().toString() + "\n");
+        builder.append("Judul Buku : ");
+        builder.append(tvHasilsp.getText().toString() + "\n");
+        builder.append("Kondisi Pinjam : ");
+        builder.append(tvHasilcb.getText().toString() + "\n");
+
+        tvHasil.setText(builder);
+
+        /* if(rb.gtText().toString().equals("member")) diskon = 10;
+         * else if */
+
         String hasilrb = null;
 
         if (rbSM.isChecked()) {
@@ -65,13 +110,13 @@ public class MainActivity extends AppCompatActivity {
             hasilrb = rbPM.getText().toString();
         }
 
-        if (hasilrb == null) {
+        /*if (hasilrb == null) {
             tvHasil.setText("Belum Menjadi Member");
         } else {
             tvHasil.setText("\n Member Anda : " + hasilrb + "\n");
         }
 
-        tvHasil.setText("Judul Buku : " + spJudul.getSelectedItem().toString() + "\n");
+        /*tvHasil.setText("Judul Buku : " + spJudul.getSelectedItem().toString() + "\n");*/
 
         String hasilcb = "Kondisi Buku Pinjam : \n";
         int startlen = hasilcb.length();
@@ -81,18 +126,13 @@ public class MainActivity extends AppCompatActivity {
 
         if (hasilcb.length() == startlen) hasilcb += "Tidak Meminjam Buku";
 
-        tvHasil.setText(hasilcb);
+        tvHasilcb.setText(hasilcb);
     }
 
 
     private void doProcess() {
-        if (isValid()) {
-            String nama = etNama.getText().toString();
-            String alamat = etAlamat.getText().toString();
-            String ktp = etKtp.getText().toString();
 
-            tvHasil.setText("Nama : " + nama + "\n Alamat :" + alamat + "\n No Ktp : " + ktp);
-        }
+
     }
 
     private boolean isValid() {
